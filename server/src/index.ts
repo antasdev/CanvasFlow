c8f5235c6fab122598226c6d650b78c11a7dd10d
antasantony <antonyantas03@gmail.com>
2026-07-28 20:12:33 +0530

    feat(database): add MongoDB connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,27 @@
-import dotenv from "dotenv";
 import app from "./app";
+import env from "./config/env";
+import connectDatabase from "./config/database";
 
-dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const startServer = async ():Promise<void>=>{
+  await connectDatabase();
+  app.listen(env.PORT,()=>{
+    console.log(`server running on http://localhost:${env.PORT}`);
+  });
+};
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+startServer();
+
+import mongoose from "mongoose";
+
+// Listen for the SIGINT signal (triggered when we press Ctrl + C)
+process.on("SIGINT", async () => {
+  // Close the MongoDB connection gracefully before shutting down
+  await mongoose.connection.close();
+
+  // Log a message to confirm the database connection is closed
+  console.log("🛑 MongoDB connection closed");
+
+  // Exit the Node.js process with status code 0 (successful shutdown)
+  process.exit(0);
 });
